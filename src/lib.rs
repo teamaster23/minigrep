7e4b0f06f99403ece840a5c282b048581872d1fd
teamaster23 <3132789819@qq.com>
2025-06-29 16:26:07 +0800

    finish

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
+//use std::env; 
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub case_insensitive: bool, // 是否忽略大小写
 }
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
@@ -11,18 +13,36 @@ impl Config {
             return Err("Not enough arguments");
         }
 
+        let mut case_insensitive = false; // 默认不忽略大小写
+
+        if args.len() == 4{
+            if args[3] == "1" {
+                case_insensitive = true;
+            }
+            else {
+                case_insensitive = false;
+            }
+        }
+
         let query = args[1].clone(); // 第一个参数，通常是要搜索的字符串
         let file_path = args[2].clone(); // 第二个参数，通常是要搜索的文件路径
         
-        return Ok(Config { query, file_path});
+        return Ok(Config { query, file_path, case_insensitive });
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
     
+    let results = if config.case_insensitive {
+        //println!("有设置忽略大小写");
+        
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
     //println!("With text:\n{}", contents);
-    for line in search(&config.query, &contents) {
+    for line in results {
         println!("{line}");
     }
     return Ok(());
@@ -35,6 +55,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
             results.push(line);
         }
     }
+    return results;
+}
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    let query = query.to_lowercase(); // 将查询字符串转换为小写
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
     return results;
 }
 
@@ -53,4 +86,14 @@ Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
+    #[test]
+    fn case_insensitive() {
+        let query = "RUST";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main(){
     let args: Vec<String> = env::args().collect();//包含程序启动时的所有命令行参数，并返回一个字符串向量
     
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     }); // 解析命令行参数，获取查询字符串和文件路径
 
@@ -16,7 +16,7 @@ fn main(){
     println!("Searching for '{}' in file '{}'", config.query, config.file_path);
 
     if let Err(e) = minigrep::run(config){
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }; // 调用 run 函数来处理文件内容
 }
